1dc91d33ee4977509790e114767e7415edffbe6d
Faruq <olowonlafaruq@gmail.com>
2024-10-10 08:46:17 -0700

    change

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware Setup
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'smallblog-beta.vercel.app',
     credentials: true,
 }));
 
